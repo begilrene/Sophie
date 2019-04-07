b3fcb343ebdb65e9fb752a83fdaa98af9b6c20d7
Begilrene <begil@cpp.edu>
2019-04-06 23:12:43 -0700

    Basic directional input, graphics.

---
+++ Assets/Scripts/BeatScroller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatScroller : MonoBehaviour
+{
+
+    public float beatTempo; //how fast arrows fall down
+    public bool hasStarted; //Press a button to make things fall down the screen.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        beatTempo = beatTempo / 60f; //setting how fast the scroll should be per second.
+                                       // at 120bpm, you'll move at two units (beats) per second.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!hasStarted)
+        {
+            if(Input.anyKeyDown)
+            {
+                hasStarted = true;
+            }
+        }
+        else
+        {
+            transform.position -= new Vector3(0f, beatTempo * Time.deltaTime, 0f);
+        }
+    }
+}
