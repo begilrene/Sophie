0b8c30f44ef3f676221d0b7a1ccdbe92a9c5c4c5
Begil <begil@cpp.edu>
2019-04-13 19:34:15 -0700

    Tok: Start Menu Functional

---
+++ Assets/Scripts/Scene_Changer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Changer : MonoBehaviour
+{
+
+    public string sceneName;
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
